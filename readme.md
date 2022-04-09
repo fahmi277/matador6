a3c59d2c5c7dbaa9ae11df48e63e74cd9c8359fe
fahmi277 <fahmi.sangengon@gmail.com>
2022-04-09 15:19:07 +0700

    push code 6

---
+++ readme.md
@@ -0,0 +1,14 @@
+project : MQTT windows dan Nodered (Menyusul)
+
+
+Bahan : ESP32, INA219, Internal SUhu ESP32
+
+Note : ESP32 terbaru tidak mendukung sensor internal
+
+
+Library :
+INA219 by Adafruit
+
+
+Source :
+Rui Santos : Complete project details at https://RandomNerdTutorials.com/esp32-mqtt-publish-ds18b20-temperature-arduino/
---
+++ widya_matador_nomor_6.ino
@@ -0,0 +1,226 @@
+
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+
+#include <WiFi.h>
+extern "C"
+{
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+}
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+  uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+#include <AsyncMqttClient.h>
+
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+// Raspberry Pi Mosquitto MQTT Broker
+#define MQTT_HOST IPAddress(192, 168, 1, 123)
+// For a cloud MQTT broker, type the domain name
+//#define MQTT_HOST "example.com"
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topic
+#define MQTT_PUB_TEMP "esp32/ds18b20/temperature"
+#define MQTT_PUB_VOLTAGE "esp32/voltage/voltage"
+#define MQTT_PUB_ARUS "esp32/arus/arus"
+
+float temp;
+float voltage;
+float current;
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+unsigned long previousMillis = 0; // Stores last time temperature was published
+const long interval = 10000;      // Interval at which to publish sensor readings
+
+void connectToWifi()
+{
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt()
+{
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event)
+  {
+  case SYSTEM_EVENT_STA_GOT_IP:
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    connectToMqtt();
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    Serial.println("WiFi lost connection");
+    xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+    xTimerStart(wifiReconnectTimer, 0);
+    break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent)
+{
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected())
+  {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+/*void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}*/
+
+void onMqttPublish(uint16_t packetId)
+{
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setupINA219()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    // will pause Zero, Leonardo, etc until serial console opens
+    delay(1);
+  }
+
+  uint32_t currentFrequency;
+
+  Serial.println("Hello!");
+
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  ina219.begin();
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  // ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  // ina219.setCalibration_16V_400mA();
+
+  Serial.println("Measuring voltage and current with INA219 ...");
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  setupINA219();
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  // mqttClient.onSubscribe(onMqttSubscribe);
+  // mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  // mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
+  connectToWifi();
+}
+
+void loopINA219(void)
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  Serial.print("Bus Voltage:   ");
+  Serial.print(busvoltage);
+  Serial.println(" V");
+  Serial.print("Shunt Voltage: ");
+  Serial.print(shuntvoltage);
+  Serial.println(" mV");
+  Serial.print("Load Voltage:  ");
+  Serial.print(loadvoltage);
+  Serial.println(" V");
+  Serial.print("Current:       ");
+  Serial.print(current_mA);
+  Serial.println(" mA");
+  Serial.print("Power:         ");
+  Serial.print(power_mW);
+  Serial.println(" mW");
+  Serial.println("");
+
+  voltage = busvoltage;
+  current = current_mA / 1000;
+
+  delay(2000);
+}
+
+void loopTemperature()
+{
+temp:
+  (temprature_sens_read() - 32) / 1.8;
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds)
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval)
+  {
+    loopINA219();
+    loopTemperature();
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New temperature readings
+
+    // Publish an MQTT message on topic esp32/ds18b20/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_VOLTAGE, 1, true, String(voltage).c_str());
+    uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_ARUS, 1, true, String(current).c_str());
+  }
+}
